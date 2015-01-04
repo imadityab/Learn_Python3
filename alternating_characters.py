f085e1217a8761cec854502e3a64f857f26b4c4d
Yatish Mehta <yatishmehta27@gmail.com>
2015-01-04 15:58:46 -0500

    Adds style check for alternating_characters.py

--- alternating_characters.py
+++ alternating_characters.py
@@ -1,13 +1,13 @@
-def numdel(string):
-    count=0
-    mylist=list(string)
-    for j in range(len(string)-1):
-        if(mylist[j]==mylist[j+1]):
-            count+=1    
+def num_del(string):
+    count = 0
+    mylist = list(string)
+    for j in range(len(string) - 1):
+        if(mylist[j] == mylist[j + 1]):
+            count += 1
     return count
 
 if __name__== '__main__':
-    
+
     for i in range(int(input())):
-        test=input()
-        print(numdel(test))
+        test = input()
+        print(num_del(test))
